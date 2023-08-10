39a56639e651f6690eab3aca5a68f2a386f8abd0
zl8244 <zl8244@g.rit.edu>
2023-08-10 14:52:28 -0400

    Implemented basic difficulty selection

--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        Minesweeper game = new Minesweeper();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to Minesweeper! Please select your difficulty: ");
+        System.out.println("(E)asy\n(M)edium\n(H)ard");
+        String difficulty = scan.nextLine();
+        String[] validDiffs = {"E", "e", "M", "m", "H", "h"};
+        for(int i = 0; i < validDiffs.length; i++) {
+            if(difficulty.equals(validDiffs[i]))
+                break;
+        }
+        Minesweeper game = new Minesweeper(difficulty);
         game.runGame();
+        scan.close();
     }
 }
